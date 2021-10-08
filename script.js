654a5c492367d708a4b90b058835f5ab94a0ec75
Faizal Anwar <fzlanwr@gmail.com>
2021-10-08 09:07:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
               if(url !== null){
                    window.location.href = "http://"+url;
               }else{
-                  window.location.href = &quot;<data:blog.homepageUrl/>&quot;
+                  window.location.href = 'www.google.com'
               }
           }else{
             window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
