b2a29cc3c9c7dd501204061b2ae625b19782d56e
Faizal Anwar <fzlanwr@gmail.com>
2021-10-08 07:29:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// function fizalanwar(url){
+var flag = 0;
+$('#faizal').click(function(){
+    if(flag <= 2)
+    {
+      // window.open(url); 
+       window.location = https://goggle.com
+    }else{
+       window.location = https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55
+    }
+    flag++;
+});
