086e6dc7addbdd3383513ce7e2ac2a84aaa39688
Faizal Anwar <fzlanwr@gmail.com>
2021-10-08 07:42:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var flag = 0;
 $('#faizal').click(function(){
     if(flag <= 2)
     {
-      // window.open(url); 
-       window.location = https://goggle.com
+        
+        window.location.href = "http://www.w3schools.com";
+
     }else{
-       window.location = https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55
+        
+window.location.href = "https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55";
     }
     flag++;
 });
