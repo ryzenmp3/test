27274f10adcf400efb6e86a6d5653d8a88036fc3
Faizal Anwar <fzlanwr@gmail.com>
2021-10-08 08:46:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 jQuery(function($){
-// function fizalanwar(url){
-var flag = 0;
-$('#faizal').click(function(){
-    if(flag >= 1)
-    {
-        console.log(flag);
-        window.location.href = "http://www.w3schools.com";
-
-    }else{
-        
-        console.log(flag);
-      window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
+   var flag = 0;
+    function myFunction(url) {
+       if(flag >= 1)
+          {
+              if(url !== null){
+                   window.location.href = "http://"+url;
+              }else{
+                  var home='<data:blog.url/>';
+                  location.href = home;
+              }
+          }else{
+            window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
+          }
+          flag++;
     }
-    flag++;
-});
-
 });
