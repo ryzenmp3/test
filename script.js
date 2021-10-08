5c923e155044698aea2f977dc69922c199761d2d
Faizal Anwar <fzlanwr@gmail.com>
2021-10-08 09:33:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
  var flag = 0;
 function myFunction(url) {
-       if(flag >= 2)
+       if(flag >= 2)    // jika di klik lebih dari 2 kali
           {
-              if(url !== undefined){
+              if(url !== undefined){ // jika terdapat ada parameter d button akan redrect ke url tujuan
                    window.location.href = "http://"+url;
-              }else{
+              }else{ // jika tidak ada parameter d button akan redrect ke homepage
                   document.location.href="/";
               }
-          }else{
+          }else{ // jika di klik kurang / sama dengan 2 kali akan redirect ke url iklan
             window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
           }
           flag++;
