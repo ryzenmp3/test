83a6d649e39e8ffdef916e197b54ffee9eaa79b0
Faizal Anwar <fzlanwr@gmail.com>
2021-10-08 09:18:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-   var flag = 0;
-    function myFunction(url) {
+ var flag = 0;
+function myFunction(url) {
        if(flag >= 2)
           {
               if(url !== undefined){
                    window.location.href = "http://"+url;
               }else{
-                  window.location.href = "http://www.google.com'
+                  var home='www.google.com';
+                  location.href = "http://"+home;
               }
           }else{
             window.open("https://pubfruitlesswording.com/fy755du4v?key=7b2683f6e5e3e859cf2d2483d46f2e55");
           }
           flag++;
-    }
+}
